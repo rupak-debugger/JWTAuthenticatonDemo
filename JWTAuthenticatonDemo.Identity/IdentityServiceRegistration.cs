0a2448d95b59291d1cd3676c102d6c32af14424c
Rupak <Rupak@DESKTOP-OQ9SI51>
2023-04-16 10:48:09 +0545

    jwt settings configured partially

--- JWTAuthenticatonDemo.Identity/IdentityServiceRegistration.cs
+++ JWTAuthenticatonDemo.Identity/IdentityServiceRegistration.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
 using JWTAuthenticatonDemo.Identity.Models;
+using JWTAuthenticatonDemo.Application.Settings;
 
 namespace JWTAuthenticatonDemo.Identity
 {
@@ -15,13 +16,24 @@ namespace JWTAuthenticatonDemo.Identity
     {
         public static IServiceCollection ConfigureIdentityServices(this IServiceCollection services, IConfiguration configuration)
         {
+            //Adding DbContext service
             services.AddDbContext<ApplicationIdentityDbContext>(options =>
                 options.UseSqlServer(configuration.GetConnectionString(""),
                 b => b.MigrationsAssembly(typeof(ApplicationIdentityDbContext).Assembly.FullName)));
 
-            services.AddIdentityCore<ApplicationUser>()
+            //Adding Identity service with Application User
+            services.AddIdentityCore<ApplicationUser>(options =>
+            {
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequireDigit = false;
+                options.User.RequireUniqueEmail = true;
+            })
                 .AddRoles<IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationIdentityDbContext>();
+
+            //JWT configuration from AppSetting
+            services.Configure<JWTSettings>(configuration.GetSection(""));
 
             return services;
         }
---
+++ JWTAuthenticatonDemo.Identity/Services/AuthenticationService.cs
@@ -0,0 +1,26 @@
+﻿using JWTAuthenticatonDemo.Application.Settings;
+using JWTAuthenticatonDemo.Identity.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JWTAuthenticatonDemo.Identity.Services
+{
+    public class AuthenticationService
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly JWTSettings _jwtSettings;
+
+        public AuthenticationService(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, IOptions<JWTSettings> jwtSettings)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+            _jwtSettings = jwtSettings.Value;
+        }
+    }
+}
