f97a3a5fec491547793433366a430966d2241135
Rupak <Rupak@DESKTOP-OQ9SI51>
2023-04-19 10:45:45 +0545

    application project reference added to api project and configuration done for services

---
+++ JWTAuthenticatonDemo.API/StartupExtensions.cs
@@ -0,0 +1,24 @@
+﻿using JWTAuthenticatonDemo.Application;
+
+namespace JWTAuthenticatonDemo.API
+{
+    public static class StartupExtensions
+    {
+        public static WebApplication ConfigureServices(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddApplicationLayer(builder.Configuration);
+            builder.Services.AddControllers();
+            //builder.Services.AddCors();
+            return builder.Build();
+        }
+
+        public static WebApplication ConfigurePipeline(this WebApplication app)
+        {
+            app.UseHttpsRedirection();
+            app.UseRouting();
+            //app.UseCors();
+            app.MapControllers();
+            return app;
+        }
+    }
+}
